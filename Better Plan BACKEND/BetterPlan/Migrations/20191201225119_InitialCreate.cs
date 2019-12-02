00d4dd4575fc5596e361fcea8eb81894767533f2
Nick Taraskin <taraskin86@gmail.com>
2019-12-02 20:33:07 +0200

    Added template WEB API

---
+++ Better Plan BACKEND/BetterPlan/Migrations/20191201225119_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BetterPlan.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModelPosts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdPost = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    ReferenceImage = table.Column<string>(nullable: true),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: false),
+                    DeleteDateTime = table.Column<DateTime>(nullable: false),
+                    IsDelete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelPosts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModelPosts");
+        }
+    }
+}
---
+++ Better Plan BACKEND/BetterPlan/Models/ModelPostContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterPlan.Models
+{
+    public class ModelPostContext:DbContext
+    {
+
+        public ModelPostContext(DbContextOptions<ModelPostContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ModelPost> ModelPosts { get; set; }
+    }
+
+}
+
