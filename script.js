1edcef7062f3196c1699d8180f6058b41e7b4a5a
Olga Berezhna <berezhna.o.v@gmail.com>
2019-12-07 13:35:15 +0200

    POST json

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 new Vue({
-    el: '#app',
-    
-    data() {
-      return {
-        selectedFile: null,
-        text: '',
-        msg: 'App'
-      }
-    },
-    methods: {
-      selectFile(event) {
-        this.selectedFile = event.target.files[0]; 
-      },
-      uploadFiles() {
-        const data = new FormData();
-        data.append('image', this.selectedFile, this.selectedFile.name)
-        data.append('text', this.text)
-        axios.post('http://127.0.0.1:8080', data, {
-            headers: {
-              'Content-Type': 'multipart/form-data'
-            }
-          })
-          .then(res => {
-            console.log(res)
-            
-          })
-          .catch(error => {
-            console.log(error.res)
-          })
-      }
+  el: '#app',
+  
+  data() {
+    return {
+      text: '',
+      url: '',
+      msg: 'App'
     }
-  })
+  },
+   methods: {
+    uploadFiles() {
+      const data = new FormData();
+      data.append("post_img_url", this.url)
+      data.append("post_text", this.text)
+      let obj ={};
+      data.forEach((value, key)=> obj[key]=value);
+      axios.post('http://127.0.0.1:8080', JSON.stringify(obj))
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.log(error.res)
+        })
+    }
+  }
+})
