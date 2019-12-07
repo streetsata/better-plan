5a50d710cfe8e641306b162be8f28de25698c75a
Olga Berezhna <berezhna.o.v@gmail.com>
2019-12-07 15:44:57 +0200

    Fix JSON  generating

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ new Vue({
   },
    methods: {
     uploadFiles() {
-      const data = new FormData();
-      data.append("post_img_url", this.url)
-      data.append("post_text", this.text)
-      let obj ={};
-      data.forEach((value, key)=> obj[key]=value);
-      axios.post('http://127.0.0.1:8080', JSON.stringify(obj))
+      let obj =JSON.stringify({"post_img_url":this.url,"post_text": this.text});
+      axios.post('http://127.0.0.1:8080', obj,{
+      contentType: 'application/json'})
         .then(res => {
           console.log(res)
         })
