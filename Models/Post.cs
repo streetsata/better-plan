696576d9fa233205ed8d0e3c050298d3f0bee312
Sketch <lubkin.andrey@gmail.com>
2020-01-18 12:37:56 +0300

    fix database

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace BetterPlan.Models
 {
     public class Post
     {
-        public string Id { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Post_id { get; set; }
         public string Text { get; set; }
         public string Link { get; set; }
         public string Place { get; set; }
