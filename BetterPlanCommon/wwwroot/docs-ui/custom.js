cef88f91cd468dcd0f520aebe6b29ecc79c141f6
Deniz <denizigra@gmail.com>
2020-03-09 14:27:45 +0200

    Delete garbage

--- BetterPlanCommon/wwwroot/docs-ui/custom.js
+++
@@ -1,9 +0,0 @@
-console.log("Здесь был Скотч");
-(function () { 
-    window.addEventListener("load", function () {
-        setTimeout(function () {
-            var logo = document.getElementsByClassName('link');    
-            logo[0].innerHTML = "BetterPlan Api-Docs";  
-        });
-    });
-})();
