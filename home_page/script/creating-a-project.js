fb3f302710012883093da6da2c50cd3015865992
zinnervitaliy <zinnervitaliy@gmail.com>
2020-04-30 17:50:38 +0300

    added "creating-a-project.html'

---
+++ home_page/script/creating-a-project.js
@@ -0,0 +1,28 @@
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+}
+
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+// }
