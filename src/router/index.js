e8b6f6983333d03afe27dbbb012b24195dcf2c2d
SergArtem <streetsata@live.ru>
2020-06-08 23:07:21 +0300

    fix2

--- src/router/index.js
+++ src/router/index.js
@@ -10,39 +10,37 @@ import StartPage from '../views/StartPage';
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'FacebookWorkspace',
-    component: FacebookWorkspace
-  },
-  {
-    path: '/moodboard',
-    name: 'Moodboard',
-    component: Moodboard
-  },
-  {
-    path: '/learning',
-    name: 'StartLearning',
-    component: StartLearning
-  },
-  {
-<<<<<<< HEAD
-    path: '*',
-    name: 'NotFound',
-    component: NotFound
-=======
-    path: '/slider',
-    name: 'StartPage',
-    component: StartPage
->>>>>>> de75fabb8633641178b14a7036b79e43a612329f
-  }
-  
+    {
+        path: '/',
+        name: 'FacebookWorkspace',
+        component: FacebookWorkspace
+    },
+    {
+        path: '/moodboard',
+        name: 'Moodboard',
+        component: Moodboard
+    },
+    {
+        path: '/learning',
+        name: 'StartLearning',
+        component: StartLearning
+    },
+    {
+        path: '*',
+        name: 'NotFound',
+        component: NotFound
+    },
+    {
+        path: '/slider',
+        name: 'StartPage',
+        component: StartPage
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
